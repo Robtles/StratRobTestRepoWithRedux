e31fa3445bde8b6e5b56e22de96f76b46accc6fe
Rob <robin.dreux@bruce.work>
2019-03-07 16:10:39 +0100

    Base almost ready

--- StratRobTestRepoWithRedux/Application/Stage/Shape/Application/ShapeApplicationCompass.swift
+++ StratRobTestRepoWithRedux/Application/Stage/Shape/Application/ShapeApplicationCompass.swift
@@ -15,7 +15,7 @@ public typealias Compass = ShapeApplicationCompass
 
 // MARK: - Type
 
-open class ShapeApplicationCompass: UITabBarController, UITabBarControllerDelegate, StoreSubscriber, CompassProtocol {
+open class ShapeApplicationCompass: UITabBarController, UINavigationControllerDelegate, UITabBarControllerDelegate, StoreSubscriber, CompassProtocol {
 
     // MARK: - Static Properties
     
@@ -38,14 +38,11 @@ open class ShapeApplicationCompass: UITabBarController, UITabBarControllerDelega
 
     // MARK: Life Methods
     
-    open override func setViewControllers(_ viewControllers: [UIViewController]?, animated: Bool) {
-        super.setViewControllers(tabBarSections, animated: animated)
-    }
-    
     override open func viewDidLoad() {
         super.viewDidLoad()
-        self.setViewControllers(tabBarSections, animated: true)
-        self.tabBar.isHidden = true
+        self.setNeedsStatusBarAppearanceUpdate()
+        self.delegate = self
+        self.updateContent()
     }
     
     // MARK: Navigation Methods
@@ -67,7 +64,26 @@ open class ShapeApplicationCompass: UITabBarController, UITabBarControllerDelega
     // MARK: Store Methods
     
     open func newState(state: Store) {}
-
+ 
+    // MARK: Tab Bar Methods
+    
+    open func updateContent() {
+        DispatchQueue.main.async {
+            self.tabBarSections.forEach { section in
+                section.delegate = self
+            }
+            self.tabBar.isHidden = self.tabBarShouldBeHidden
+            self.setViewControllers(self.tabBarSections, animated: false)
+            self.tabBar.items?.enumerated().forEach({ index, tabBarItem in
+                tabBarItem.image = self.tabBarSectionImages[index].withRenderingMode(.alwaysOriginal)
+                tabBarItem.selectedImage = self.tabBarSectionSelectedImages[index].withRenderingMode(.alwaysOriginal)
+                tabBarItem.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+                tabBarItem.setTitleTextAttributes([.foregroundColor: UIColor.clear], for: .selected)
+                tabBarItem.setTitleTextAttributes([.foregroundColor: UIColor.clear], for: .normal)
+            })
+        }
+    }
+    
 }
 
 
@@ -76,12 +92,30 @@ public protocol CompassProtocol {
     
     var tabBarSections: [ShapeControllersSection] { get }
     
+    var tabBarSectionImages: [UIImage] { get }
+    
+    var tabBarSectionSelectedImages: [UIImage] { get }
+    
+    var tabBarShouldBeHidden: Bool { get }
+    
 }
 
 extension CompassProtocol {
     
     public var tabBarSections: [ShapeControllersSection] {
         return []
+    }
+    
+    public var tabBarSectionImages: [UIImage] {
+        return []
+    }
+    
+    public var tabBarSectionSelectedImages: [UIImage] {
+        return []
+    }
+    
+    public var tabBarShouldBeHidden: Bool {
+        return false
     }
     
 }

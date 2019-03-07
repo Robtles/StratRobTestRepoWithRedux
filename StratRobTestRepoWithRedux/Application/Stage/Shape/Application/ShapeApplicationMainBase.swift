ab632bd83d64c47b3c2bfdb0d357f2cd0d3d6e22
Rob <robin.dreux@bruce.work>
2019-03-07 16:24:46 +0100

    Next release

---
+++ StratRobTestRepoWithRedux/Application/Stage/Shape/Application/ShapeApplicationMainBase.swift
@@ -0,0 +1,63 @@
+//
+//  ShapeApplicationMainBase.swift
+//  StratRobTestRepoWithRedux
+//
+//  Created by Rob on 07/03/2019.
+//  Copyright © 2019 com.rob. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ShapeApplicationMainBase: UIResponder, UIApplicationDelegate {
+    
+    // MARK: View Properties
+    
+    static var rootViewController = UIApplication.shared.delegate?.window??.rootViewController
+    
+    var window: UIWindow?
+    
+    // MARK: Life Methods
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        defer {
+            self.window = UIWindow(frame: UIScreen.main.bounds)
+            Compass.shared.updateContent()
+            self.defaultLaunch()
+        }
+        return true
+    }
+    
+    // MARK: View Methods
+    
+    func defaultLaunch() {
+        self.replace(with: Compass.shared)
+    }
+    
+    func replace(with viewController: UIViewController) {
+        defer {
+            window?.makeKeyAndVisible()
+        }
+        if Thread.isMainThread {
+            window?.rootViewController = viewController
+        } else {
+            DispatchQueue.main.async {
+                self.window?.rootViewController = viewController
+            }
+        }
+    }
+    
+    static var topMostViewController: UIViewController? {
+        guard let windowRootViewController = UIApplication.shared.delegate?.window??.rootViewController else {
+            return nil
+        }
+        return ShapeApplicationMainBase.topMostViewController(windowRootViewController)
+    }
+    
+    // Will find recursively the top-most presented view controller to avoid "already presenting view controller" issue
+    private static func topMostViewController(_ parentViewController: UIViewController) -> UIViewController {
+        return parentViewController.presentedViewController == nil ? parentViewController : ShapeApplicationMainBase.topMostViewController(parentViewController.presentedViewController!)
+    }
+    
+}
